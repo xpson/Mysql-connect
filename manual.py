01457ccd5171f640ea3a7e483c1d61212c1a707b
sean <sunxpeng@gmail.com>
2015-04-09 11:12:56 +0800

    class

--- manual.py
+++ manual.py
@@ -1,2 +1,10 @@
 __author__ = 'Administrator'
 print 'hello'
+class FirstClass:
+    def setdate(self,value):
+        self.data=value
+    def display(self):
+        print(self.data)
+x = FirstClass()
+x.setdate('Kingarthur')
+x.display()
